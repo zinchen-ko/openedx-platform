db74d48ef1b21557d097bbf2e9cc17e721f9edcc
Farhaan Bukhsh <farhaan@opencraft.com>
2023-01-04 00:57:22 +0530

    fix: Optimize serializer for enrollment endpoint.
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- openedx/core/djangoapps/enrollments/serializers.py
+++ openedx/core/djangoapps/enrollments/serializers.py
@@ -87,48 +87,14 @@ class CourseEnrollmentSerializer(serializers.ModelSerializer):
     """
     course_details = CourseSerializer(source="course_overview")
     user = serializers.SerializerMethodField("get_username")
-    finished = serializers.SerializerMethodField()
-    grading = serializers.SerializerMethodField()
 
     def get_username(self, model):
         """Retrieves the username from the associated model."""
         return model.username
 
-    def get_finished(self, model):
-        """Retrieve finished course."""
-        course = modulestore().get_course(model.course_id)
-        if course:
-            try:
-                coursegrade = CourseGradeFactory().read(model.user, course).passed
-            except PermissionDenied:
-                return False
-            return coursegrade
-        return False
-
-    def get_grading(self, model):
-        """Retrieve course grade."""
-        course = modulestore().get_course(model.course_id)
-        course_grade = None
-        summary = []
-        current_grade = 0
-        if course:
-            try:
-                course_grade = CourseGradeFactory().read(model.user, course)
-                current_grade = int(course_grade.percent * 100)
-                for section in course_grade.summary.get(u'section_breakdown'):
-                    if section.get(u'prominent'):
-                        summary.append(section)
-            except PermissionDenied:
-                pass
-        return [
-            {u'current_grade': current_grade,
-             u'certificate_eligible': course_grade.passed if course_grade else False,
-             u'summary': summary}
-        ]
-
     class Meta(object):
         model = CourseEnrollment
-        fields = ('created', 'mode', 'is_active', 'course_details', 'user', 'finished', 'grading')
+        fields = ('created', 'mode', 'is_active', 'course_details', 'user')
         lookup_field = 'username'
 
 
@@ -138,13 +104,51 @@ class CourseEnrollmentsApiListSerializer(CourseEnrollmentSerializer):
     by the CourseEnrollmentSerializer.
     """
     course_id = serializers.CharField(source='course_overview.id')
+    finished = serializers.SerializerMethodField()
+    grading = serializers.SerializerMethodField()
+    course_grade = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields.pop('course_details')
 
+    def _get_course_grade(self, model):
+        if self.course_grade == None:
+            course = modulestore().get_course(model.course_id)
+            if course:
+                try:
+                    self.course_grade = CourseGradeFactory().read(model.user, course)
+                except PermissionDenied:
+                    self.course_grade = False
+            else:
+                self.course_grade = False
+        return self.course_grade
+
+    def get_finished(self, model):
+        """Retrieve finished course."""
+        course_grade = self._get_course_grade(model)
+        if course_grade:
+            course_grade = course_grade.passed
+        return course_grade
+
+    def get_grading(self, model):
+        """Retrieve course grade."""
+        summary = []
+        current_grade = 0
+        course_grade = self._get_course_grade(model)
+        if course_grade:
+            current_grade = int(course_grade.percent * 100)
+            for section in course_grade.summary.get(u'section_breakdown'):
+                if section.get(u'prominent'):
+                    summary.append(section)
+        return [
+            {u'current_grade': current_grade,
+             u'certificate_eligible': course_grade.passed if course_grade else False,
+             u'summary': summary}
+        ]
+
     class Meta(CourseEnrollmentSerializer.Meta):
-        fields = CourseEnrollmentSerializer.Meta.fields + ('course_id', )
+        fields = CourseEnrollmentSerializer.Meta.fields + ('course_id', 'finished', 'grading')
 
 
 class ModeSerializer(serializers.Serializer):  # pylint: disable=abstract-method
